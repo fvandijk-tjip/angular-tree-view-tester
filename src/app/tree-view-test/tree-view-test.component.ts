c5d5d0d0e7fbaf2d6c4d25e7cb8a1c765bb2c16f
fvandijk-tjip <f.vandijk@tjip.com>
2019-07-29 17:45:07 +0200

    dummy input moved to seperate file nodes.ts

--- src/app/tree-view-test/tree-view-test.component.ts
+++ src/app/tree-view-test/tree-view-test.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TreeModule } from 'angular-tree-component';
 
+import { nodes } from '../nodes'
+
 @Component({
   selector: 'app-tree-view-test',
   templateUrl: './tree-view-test.component.html',
@@ -8,35 +10,13 @@ import { TreeModule } from 'angular-tree-component';
 })
 export class TreeViewTestComponent implements OnInit {
 
+  nodes = nodes;
   
   constructor() { }
 
   ngOnInit() {
   }
 
-nodes = [
-    {
-      id: 1,
-      name: 'root1',
-      children: [
-        { id: 2, name: 'child1' },
-        { id: 3, name: 'child2' }
-      ]
-    },
-    {
-      id: 4,
-      name: 'root2',
-      children: [
-        { id: 5, name: 'child2.1' },
-        {
-          id: 6,
-          name: 'child2.2',
-          children: [
-            { id: 7, name: 'subsub' }
-          ]
-        }
-      ]
-    }
-  ];
+
   options = {};
 }
